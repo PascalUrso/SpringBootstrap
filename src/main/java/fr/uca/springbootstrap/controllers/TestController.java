751aa136c192db34eb28569ce0c73d244be61a8b
PascalUrso <pascal.urso@gmail.com>
2022-02-09 18:55:48 +0100

    Back to GET /api/test/all

--- src/main/java/fr/uca/springbootstrap/controllers/TestController.java
+++ src/main/java/fr/uca/springbootstrap/controllers/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
-	@PostMapping("/all")
+	@GetMapping("/all")
 	public String allAccess() {
 		return "Public Content.";
 	}
